01b3dcbd4002172017000da5ffa430341dfce0ec
Payisun <96931984+Payisun@users.noreply.github.com>
2022-01-20 19:08:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ get_queue = {}
 BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "90"))
 ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
@@ -20,7 +20,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NotReallyShikhar/YukkiMusicBot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Payisun/YukkiMusicBot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
